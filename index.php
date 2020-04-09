1a04166b10bb0d5bf7ddc501c8cfd1db6db9eebb
shira-maz <SHIRAMAZARI50@GMAIL.COM>
2020-04-09 17:10:48 +0300

    First

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html class="loading" lang="en" data-textdirection="rtl">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <title>KidsCare-Login</title>
+    <link rel="apple-touch-icon" href="app-assets/images/ico/logo.png">
+    <link rel="shortcut icon" type="image/x-icon" href="app-assets/images/ico/logo.png">
+ 
+    <link rel="stylesheet" type="text/css" href="app-assets/vendors/css/vendors-rtl.min.css">
+    <link rel="stylesheet" type="text/css" href="app-assets/css-rtl/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="app-assets/css-rtl/bootstrap-extended.css">
+    <link rel="stylesheet" type="text/css" href="app-assets/css-rtl/colors.css">
+    <link rel="stylesheet" type="text/css" href="app-assets/css-rtl/components.css">
+    <link rel="stylesheet" type="text/css" href="app-assets/css-rtl/custom-rtl.css">
+    <link rel="stylesheet" type="text/css" href="app-assets/css-rtl/core/menu/menu-types/vertical-menu.css">
+    <link rel="stylesheet" type="text/css" href="app-assets/css-rtl/core/colors/palette-gradient.css">
+    <link rel="stylesheet" type="text/css" href="app-assets/css-rtl/pages/login-register.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style-rtl.css">
+</head>
+
+<body class="vertical-layout vertical-menu 1-column  bg-full-screen-image blank-page blank-page" data-open="click" data-menu="vertical-menu" data-color="bg-gradient-x-purple-blue" data-col="1-column">
+        <div class="app-content content">
+        <div class="content-wrapper">
+            <div class="content-wrapper-before"></div>
+            <div class="content-header row">
+            </div>
+            <div class="content-body">
+                <section class="flexbox-container">
+                    <div class="col-12 d-flex align-items-center justify-content-center">
+                        <div class="col-lg-4 col-md-6 col-10 box-shadow-2 p-0">
+                            <div class="card border-grey border-lighten-3 px-1 py-1 m-0">
+                                <div class="card-header border-0">
+                                    <div class="text-center mb-1">
+                                        <img src="app-assets/images/logo/logo.png" alt="branding logo">
+                                    </div>
+                                    <h1 class="card-subtitle text-muted text-center font-small-6 mx-2 my-1"> <a href="ParentKidsCare\index.php" class="card-link">התחבר כהורה</a></h1>
+                                    <h1 class="card-subtitle text-muted text-center font-small-6 mx-2 my-1"> <a href="TeacherKidsCare\index.php" class="card-link">התחברי כגננת</a></h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+    <script src="app-assets/vendors/js/vendors.min.js" type="text/javascript"></script>
+    <script src="app-assets/vendors/js/forms/validation/jqBootstrapValidation.js" type="text/javascript"></script>
+    <script src="app-assets/js/core/app-menu.js" type="text/javascript"></script>
+</body>
+</html>
